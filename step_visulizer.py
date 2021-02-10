ac3fc941328dda00923ddc7c4f0e9564f7825ccc
Zhenning Zhang <frankzhang930@gmail.com>
2021-02-09 23:48:16 -0500

    displaying population

--- step_visulizer.py
+++ step_visulizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from colony import Colony
+import cv2
 
 color_dict = {1: (0, 0, 255), 3: (255, 0, 0)}
 
@@ -8,8 +9,10 @@ def convert_step_to_array(colony: Colony, multiplier: float = 30.0):
     Visualize a single step in colony.
     """
     # create an RGB array
-    frame = np.zeros((int((colony.height) * multiplier), int((colony.width) * multiplier), 3), dtype=np.uint8)
-    frame[:,:,:] = 255
+    frame_height = int((colony.height) * multiplier) 
+    frame_width = int((colony.width) * multiplier)
+    frame = np.full((frame_height, frame_width, 3), 255, dtype=np.uint8)
+    #frame[:,:,:] = 255
 
     spore_side = int(multiplier)
 
@@ -26,4 +29,15 @@ def convert_step_to_array(colony: Colony, multiplier: float = 30.0):
         frame[y_start:y_end, x_start:x_end] = color_dict[top_spore.sex]
         #frame[x_start:x_end, y_start:y_endd] = (0, 0, 255)
 
-    return frame
+    # displaying info 
+    below_addon = np.full((int(frame_height * 0.2), frame_width, 3), 220, dtype=np.uint8)
+    
+    cv2.putText(below_addon, 
+        "Colony Size: {}".format(len(colony.spores)),  # text
+        (20, 50),  # pos
+        cv2.FONT_HERSHEY_SIMPLEX, # font
+        2, # scale
+        (0, 0, 0), # front color
+        2)  # linetype
+
+    return np.concatenate([frame, below_addon], axis=0)
