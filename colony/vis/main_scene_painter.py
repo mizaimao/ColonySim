1ec6ad291979c1a2077edc0e57bd73256af45210
mizaimao <franknickzhang@gmail.com>
2022-06-18 19:17:35 -0400

    abstracted main scence painter

---
+++ colony/vis/main_scene_painter.py
@@ -0,0 +1,45 @@
+"""Helps drawing main scene.
+"""
+import numpy as np
+from typing import Callable, Dict, List, Tuple, Union
+
+from colony.characters.colony import Colony
+from colony.vis.colony_viewers import ColonyView, ColonyView2D, ColonyViewIso
+
+# color of players in BGR
+color_dict: Dict[int, Tuple[int, ...]] = {1: (245, 158, 66), 3: (95, 95, 250)}
+
+available_painters: Dict[str, ColonyView] = {
+    "2D": ColonyView2D,
+    "isometric": ColonyViewIso,
+}
+
+class MainScenePainter:
+    def __init__(self, style: str, colony: Colony, bitmap: np.ndarray, width: int, height: int):
+        self.colony: Colony = colony
+        self.bitmap: np.ndarray = bitmap
+
+        # check and setup painter
+        assert style in available_painters, f"{style} not supported."
+        painter_class = available_painters[style]
+        self.painter = painter_class(self.colony.width, self.colony.height, width, height, self.bitmap)
+
+        self.static_frame = self.painter.get_static_frame()
+        self.painter.paint_playground()
+
+    def paint_main_scence(self) -> np.ndarray:
+        # make a copy of raw background
+        frame = self.static_frame.copy()
+        # need to sort spores to paint them in a correct order, this is needed for isometrci viewing
+        _tempList = sorted(self.colony.step.keys(), key=lambda x: x[0], reverse=True)
+        step_sorted = sorted(_tempList, key=lambda x: x[1])
+        # paint all spores
+        for (x, y) in step_sorted:
+            spores = self.colony.step[(x, y)]
+            # show only the first spore
+            top_spore = self.colony.spores[spores[0]]
+            # dye this block
+            splore_color = color_dict[top_spore.sex]
+            self.painter.paint_large_pixel(frame, x, y, splore_color)
+
+        return frame
--- colony/vis/step_visulizer.py
+++ colony/vis/step_visulizer.py
@@ -6,16 +6,12 @@ from colony.configs.map_generator.ref import map_ref
 from colony.characters.colony import Colony
 from colony.vis.population_plotter import PopulationCurve
 from colony.configuration import MapSetup, map_cfg, world_cfg, WorldSetup
-from colony.vis.colony_viewers import ColonyView, ColonyView2D, ColonyViewIso
+
+from colony.vis.main_scene_painter import MainScenePainter
 from colony.vis.string_painter import StringPainter, add_info_to_main_pane
 
-painters: Dict[str, ColonyView] = {
-    "2D": ColonyView2D,
-    "isometric": ColonyViewIso,
-}
 
-# color of players in BGR
-color_dict: Dict[int, Tuple[int, ...]] = {1: (245, 158, 66), 3: (95, 95, 250)}
+
 
 class StepVisulizer:
     """
@@ -33,62 +29,41 @@ class StepVisulizer:
             colony: pointer to a colony object saved in memory
             pop_curve: pointer to a curve plotting object in memory
         """
-        # setup painter
+
         self.colony: Colony = colony
-        self.bitmap: np.ndarray = map_cfg.bitmap
         self.frame_height: int = colony.viewer_height
         self.frame_width: int = colony.viewer_width
+        self.bitmap: np.ndarray = map_cfg.bitmap
 
-        painter_class = painters["isometric"]
-        # painter_class = painters["2D"]
-
-        self.string_painter: StringPainter = StringPainter()
-        self.main_view_string_painter: StringPainter = None
-
-        self.painter = painter_class(
-            self.colony.width,
-            self.colony.height,
-            self.frame_width,
-            self.frame_height,
-            self.bitmap,
-        )
-        self.static_frame = self.painter.get_static_frame()
-        self.painter.paint_playground()
-
-        self.multiplier: float = self.painter.multiplier
-
-        # info pane sizes
-        self.info_pane_height: int = int(self.frame_height * 0.2)
+        # setup pane sizes
+        self.info_pane_height: int = int(self.frame_height * 0.2)  # shared by two lower panes
         self.left_info_pane_width: int = int(self.frame_width / 2)
         self.right_info_pane_width: int = self.frame_width - self.left_info_pane_width
 
-        # pointers to objects in memory
-        self.colony = colony
-        self.pop_curve = PopulationCurve(
-            width=self.right_info_pane_width, height=self.info_pane_height
-        )
+        painter_style: str = "isometric"
+        #painter_style = "2D"
+
+        # setup painters (three panes)
+        self.main_painter = MainScenePainter(  # main view (upper pane)
+            painter_style,
+            self.colony,
+            self.bitmap,
+            self.frame_width,
+            self.frame_height)
+         # colony info painter (lower left pane)
+        self.string_painter: StringPainter = StringPainter()
+        # colony debugging info painter (overlays upper pane)
+        self.main_view_string_painter: StringPainter = None
+        # pop curve painter
+        self.pop_curve = PopulationCurve(self.right_info_pane_width, self.info_pane_height)
 
     def paint_main_viewer(self, with_info: bool = False) -> np.ndarray:
         """Paint the colony main viewer, i.e. the dots and playground.
         This mutates the input frame as a result of np.array manipulation.
         """
-        # make a copy of raw background
-        frame = self.static_frame.copy()
-        # need to sort spores to paint them in a correct order, this is needed for isometrci viewing
-        _tempList = sorted(self.colony.step.keys(), key=lambda x: x[0], reverse=True)
-        step_sorted = sorted(_tempList, key=lambda x: x[1])
-        # paint all spores
-        for (x, y) in step_sorted:
-            spores = self.colony.step[(x, y)]
-            # show only the first spore
-            top_spore = self.colony.spores[spores[0]]
-            # dye this block
-            splore_color = color_dict[top_spore.sex]
-            self.painter.paint_large_pixel(frame, x, y, splore_color)
-
+        frame = self.main_painter.paint_main_scence()
         if with_info:
             self.main_view_string_painter = add_info_to_main_pane(self.main_view_string_painter, self.colony, frame, steps=5, max_rows=20)
-
         return frame
 
     def paint_info_pane(self, cycle: int) -> np.ndarray:
