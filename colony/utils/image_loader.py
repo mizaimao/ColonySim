efb9abc669d4d694275911851956e273f684cd5e
mizaimao <franknickzhang@gmail.com>
2022-06-23 22:45:03 -0400

    image loader added

--- colony/utils/image_loader.py
+++ colony/utils/image_loader.py
@@ -4,30 +4,67 @@ import os
 import numpy as np
 from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Dict, List, Optional, Tuple
 
 import cv2
 
-asset_path = Path(__file__).parent.joinpath("assets")
+
+ASSET_FOLDER = Path(__file__).parent.joinpath("../assets")
+
 
 @dataclass
-class AssetMeta:
-    
-    pass
-
-
 class ImageLoader:
-
-    def perform_image_check(self, image: np.ndarray):
-        pass
-
-    def cut_into_smaller_pieces(self, large_image: np.ndarray, ref: AssetMeta):
-        pass
-
-    def load_from_disk(self, tileset_name: str):
-        pass
+    """
+    Attributes
+        name: Name of tileset; can be different from its folder name.
+        asset_root: Relative path to tileset folder.
+        structure: files used for game structural-buildings.
+        surface: files used as floor tiles.
+        
+    """
+    name: str
+    asset_root: Path
+    structure: Dict[str, Dict[str, str]]
+    surface: Optional[Dict[str, List[Path]]] = field(default_factory=lambda: {})
+        
+    def __post_init__(self):
+        """Formating and check data correctness."""
+        assert self.name, "Make sure name is not empty."
+        self.asset_root = Path(self.asset_root)
+        for k, v in self.structure.items():
+            path, sizes = self.unpack_path_size_pair(v)
+            self.load_image_from_disk(asset_path=path, split=len(sizes))
+        for k, v in self.surface.items():
+            self.surface[k] = [Path(p) for p in v]
+        
+    @staticmethod
+    def unpack_path_size_pair(pair: Dict[str, str]) -> Tuple[Path, List[Tuple[int]]]:
+        """Unpack image path, sub-image orientation and sizes
+        from yaml parser to usable format.
+        """
+        asset_path: Path = Path(pair['path'])
+        size_str: List[str] = pair['sizes'].split(", ")
+        asset_sizes: List(Tuple[int, int]) = [
+            (int(x[0]), int(x[1])) for x in size_str
+        ]
+        return asset_path, asset_sizes
     
-    def get_raw_single(self, tile_name: str):
-        pass
-
-    def get_raw_batch(self):
-        pass
+    def load_image_from_disk(self, asset_path: Path, split: int = 1) -> List[np.ndarray]:
+        """Load one image file to memory and perform necessary checks and split.
+        """
+        image_path: Path = ASSET_FOLDER.joinpath(self.asset_root).joinpath(asset_path)
+        assert image_path.is_file(), f"Image file {image_path} not found."
+        image: np.ndarray = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
+        self.image_check(image)
+        
+        if split > 1:  # split on x-axis
+            images: List[np.ndarray] = np.split(image, split, axis=1)
+            return images
+        return [image]
+        
+    @staticmethod
+    def image_check(image: np.ndarray):
+        """Basic image validation like shape and channel."""
+        assert len(image.shape) == 3
+        h, w, c = image.shape
+        assert c == 4, "Image missing alpha channel."
