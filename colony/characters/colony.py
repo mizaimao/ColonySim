4e6d65b500d9df02b67d4a2dcc0315f4d7833709
mizaimao <franknickzhang@gmail.com>
2022-06-24 18:05:01 -0400

    revised resource addition

--- colony/characters/colony.py
+++ colony/characters/colony.py
@@ -2,7 +2,8 @@ import numpy as np
 from dataclasses import dataclass, field
 from typing import Dict, Tuple, List
 
-from colony.characters.spore import Spore
+from colony.characters.colony_stats import HappinessManager
+from colony.characters.spore import Spore, FOOD_SPEED, RES21_SPEED, RES22_SPEED, RES23_SPEED
 from colony.characters.storage import ColonyStorage
 from colony.progression.step import *
 from colony.configuration import spore_cfg, res_cfg, ResSetup
@@ -11,7 +12,6 @@ from colony.utils.info_manager import InfoManager
 
 sex_mapper = {1: "A", 3: "B"}
 INITAL_HEALTH: int = 100
-
 
 @dataclass
 class ColonyGeneralInfo:
@@ -57,6 +57,11 @@ class Colony:
         self.current_pop: int = 0
         self.current_iteration: int = 0
         self.step_record: list = [] # a record of each step  # typing???
+
+        # progress variables
+        self.tech_stage: int = 0
+        self.happiness: HappinessManager = HappinessManager(base=15.)
+        self.pop_cap: int = 40
 
         # pointers
         self.spores: Dict[int, Spore] = {} # stores all shown spores
@@ -119,6 +124,12 @@ class Colony:
         self.id_counter += 1
         self.current_pop += 1
 
+    def _remove_spore(self, spore_id: int):
+        """Remove a spore from colony (e.g. death).
+        """
+        self.info.gender_counts[self.spores[spore_id].sex] -= 1
+        del self.spores[spore_id]
+        self.current_pop -= 1
 
     def _check_die_out(self) -> bool:
         """Checks if a colony reaches certerion that do not permit progression.
@@ -130,6 +141,42 @@ class Colony:
         # check distinguishing by numbers
         return len(self.spores) >= 2
 
+    def calculate_current_step(self):
+        """Calculate resource input and output of this step. Spores dead will also be removed. 
+        Should be called before progressing to the next step.
+        Differences are this function computes the current step, while "progress_a_step" projects
+        the next iteration.
+        """
+        # add resources gathered by spores
+        res: Dict[int, int] = self.storage.res
+        res[11] += len(self.spores) * FOOD_SPEED[self.tech_stage]  # food
+        res[21] += len(self.spores) * RES21_SPEED[self.tech_stage]
+        res[22] += len(self.spores) * RES22_SPEED[self.tech_stage]
+        res[23] += len(self.spores) * RES23_SPEED[self.tech_stage]
+
+
+
+        # process spores by tile
+        for coor, spores_in_tile in self.step.items():
+            # process each spore on this tile
+            for spore_inlist_id, spore_id in enumerate(spores_in_tile):
+                s: Spore = self.spores[spore_id]
+                # spore consumes resource (food)
+                if self.storage.res[11] > 0:  # food
+                    self.storage.res[11] -= 1
+                else:
+                    s.health -= self.rng.uniform(1, 20)  # food shortage, hurting health
+                
+                # spore ages
+                s.age += 1
+
+                # spore health check
+                if s.health <= 0:
+                    self._remove_spore(spore_id)
+                    del self.step[coor][spore_inlist_id]
+                    if (self.step[coor]) == 0:
+                        del self.step[coor]
+
 
     def progress_a_step(self):
         """
@@ -138,6 +185,9 @@ class Colony:
         Returns:
             bool: if the colony dies.
         """
+        # calculate the current step, because progression will be based it results of current step
+        self.calculate_current_step()
+
         # save current step
         if self.enable_history:
             self.step_record.append(self.step.copy())
@@ -159,6 +209,7 @@ class Colony:
             # process each spore on this tile
             for spore_id in spores_in_tile:
                 
+                # spore movement
                 new_coor = get_next_coor(
                     next_directions[spore_counter],
                     coor,
@@ -170,7 +221,6 @@ class Colony:
                 if new_coor not in new_step:
                     new_step[new_coor] = []
                 new_step[new_coor].append(spore_id)
-
                 spore_counter += 1
 
         # for coor, (survival_dict, new_born) in events.items():
@@ -184,6 +234,8 @@ class Colony:
         #         self.printer.info(f"A new baby was born, new pop: {self.current_pop}.")
 
         self.step = new_step
+
+
         self.current_iteration += 1
 
         return True
--- colony/characters/spore.py
+++ colony/characters/spore.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+from typing import Tuple
+
+FOOD_SPEED: Tuple[int, ...] = (2, 4, 8, 12)
+RES21_SPEED: Tuple[int, ...] = (1, 2, 3, 4)
+RES22_SPEED: Tuple[int, ...] = (1, 2, 3, 4)
+RES23_SPEED: Tuple[int, ...] = (0, 2, 3, 4)
+
 
 @dataclass
 class Spore:
