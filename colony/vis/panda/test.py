207057cc7a74ca5c714a924ae10b35e3b2a5e96d
mizaimao <frankzhang930@gmail.com>
2022-08-07 13:16:31 -0400

    lighting and key mapping

--- colony/vis/panda/test.py
+++ colony/vis/panda/test.py
@@ -1,9 +1,11 @@
+import sys
 from typing import List, Sequence, Tuple
 
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import WindowProperties, GeomNode, NodePath
+from panda3d.core import AmbientLight, DirectionalLight, Vec4, WindowProperties, GeomNode, NodePath
 
 from colony.configuration import visual_cfg
+from colony.configs.map_generator.ref import map_ref
 from colony.vis.panda.cube import make_a_cube
 
 
@@ -27,13 +29,19 @@ class PandaViewer(ShowBase):
         """Constructor."""
         super().__init__(self)
 
+        self.root = self.render.attachNewNode("Root")
+        self.root.setPos(0.0, 0.0, 0.0)
+
         self._init_window()  # window settings
         self._init_camera()  # camera settings
+        self._init_lighting()
         self._init_graphics()  # make graphic-related changes
         self._init_keys()  # key mapping
         self.paint_playground()  # add map and basic objects to screeen
         self.test_location()
-        #self.disableMouse()
+
+        # this is needed in order to control camera by keyboard
+        self.disableMouse()
 
     def _init_window(self):
         # setup window properties
@@ -42,13 +50,43 @@ class PandaViewer(ShowBase):
         self.win.requestProperties(properties)
 
     def _init_camera(self):
-        self.camera.setPos(0, -100, 0)
+        self.camera.setPos(0, 0, 0)
         self.camera.lookAt(0.0, 0.0, 0.0)
+        self.camLens.setNearFar(1.0, 50.0)
+        self.camLens.setFov(45.0)
         # Tilt the camera down by setting its pitch.
         #self.camera.setP(-90)
 
+    def _init_lighting(self):
+        self.ambientLight = self.render.attachNewNode(
+            AmbientLight("ambient light")
+        )
+        self.ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+        self.render.setLight(self.ambientLight)
+
+        self.mainLight = self.render.attachNewNode(
+            DirectionalLight("main light")
+        )
+        # Turn it around by 45 degrees, and tilt it down by 45 degrees
+        self.mainLight.setHpr(45, -45, 0)
+        self.render.setLight(self.mainLight)
+
+    def _init_keys(self):
+        self.accept("d", self.move, [1.0, 0.0, 0.0])
+        self.accept("a", self.move, [-1.0, 0.0, 0.0])
+        self.accept("w", self.move, [0.0, 1.0, 0.0])
+        self.accept("s", self.move, [0.0, -1.0, 0.0])
+        self.accept("e", self.move, [0.0, 0.0, 1.0])
+        self.accept("q", self.move, [0.0, 0.0, -1.0])
+
     def _init_graphics(self):
+        
         pass
+
+    def move(self, x, y, z):
+        self.camera.setX(self.camera.getX() + x)
+        self.camera.setY(self.camera.getY() + y)
+        self.camera.setZ(self.camera.getZ() + z)
 
     def paint_playground(self):
         # setup background
@@ -56,23 +94,19 @@ class PandaViewer(ShowBase):
 
 
     def test_location(self):
-        # snode: GeomNode = make_a_cube()
-        
-        # cube: NodePath = self.render.attachNewNode(snode)
-        # # OpenGl by default only draws "front faces" (polygons whose vertices are
-        # # specified CCW).
         scalar: float = 1.
 
-        # cube.setTwoSided(True)
-        # cube.setPos(0, 0, 0)
-        # cube.setScale(scalar, scalar, scalar)
+        snode: GeomNode = make_a_cube()
+        cube: NodePath = self.render.attachNewNode(snode)
+        # OpenGl by default only draws "front faces" (polygons whose vertices are
+        # specified CCW).
+        cube.setTwoSided(True)
+        cube.setPos(0, 10, 0)
+        cube.setScale(scalar, scalar, scalar)
 
-        snode2: GeomNode = make_a_cube()
-        cube2: NodePath = self.render.attachNewNode(snode2)
-        cube2.setTwoSided(True)
-        cube2.setPos(0, 10, 0)
-        cube2.setScale(scalar, scalar, scalar)
-
+        # test color setup
+        c: Tuple[int, int, int] = pColor(map_ref[1][-1])
+        cube.setColor(*c)
 
 
 panda_viewer: PandaViewer = PandaViewer()
